e6494b0342ad95c29f739c263eb6261eb564c01b
Ben Starr <ben@starr>
2013-08-15 19:00:49 +1000

    Implemented recipe to install CouchDB

--- Vagrantfile
+++ Vagrantfile
@@ -6,4 +6,8 @@
 Vagrant::Config.run do |config|
   config.vm.box = "precise32"
   config.vm.box_url = "http://files.vagrantup.com/precise32.box"
+  
+  config.vm.provision :chef_solo do |chef|
+    chef.add_recipe("couchdb")
+  end
 end
--- cookbooks/couchdb/recipes/default.rb
+++ cookbooks/couchdb/recipes/default.rb
@@ -6,3 +6,20 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+execute "apt-get-update" do
+  command "apt-get update"
+end
+
+package "python-software-properties" do
+  action :install
+  options "--assume-yes"
+end
+
+execute "apt-add-repository-couchdb" do
+  command "apt-add-repository ppa:nilya/couchdb-1.3"
+end
+
+package "couchdb" do
+  action :install
+end
