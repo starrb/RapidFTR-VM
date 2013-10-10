f483987008394de8384ebe56df12101b3a8c2cb3
Ben Starr <ben@starr>
2013-10-10 18:48:42 +1000

    Run Xvfb on startup

--- cookbooks/xvfb/recipes/default.rb
+++ cookbooks/xvfb/recipes/default.rb
@@ -31,3 +31,19 @@ execute "set-display" do
   command "echo 'DISPLAY=:99' >> /etc/environment"
   not_if "cat /etc/environment | grep -q '^DISPLAY=:99$'"
 end
+
+cookbook_file "/etc/init.d/xvfb" do
+  source "xvfb"
+  owner "root"
+  group "root"
+  mode 0755
+end
+
+execute "update-rc.d-xvfb" do
+  command "update-rc.d xvfb defaults"
+end
+
+execute "start-xvfb" do
+  command "/etc/init.d/xvfb start"
+  not_if "ps ax | grep -q '[X]vfb'"
+end
